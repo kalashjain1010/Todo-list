4c17b8a79ccf96bba8f6feeccb061eb32fff370a
kalash <kalash@localhost.localdomain>
2023-09-04 22:02:43 +0530

    adding todo-list button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { FaList, FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaList, FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   let Links = [
@@ -13,28 +13,40 @@ const Navbar = () => {
 
   return (
     <>
-    <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-        {/* logo section */}
-        <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-          <FaList className='w-7 h-7' />
-          <span>Todo list </span>
+      <div className="shadow-md w-full fixed top-0 left-0">
+        <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+          {/* logo section */}
+          <div className="font-bold text-2xl cursor-pointer flex items-center gap-1">
+            <FaList className="w-7 h-7" />
+            <span>Todo list </span>
+          </div>
+          {/* Menu icon */}
+          <div
+            onClick={() => setOpen(!open)}
+            className="absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7"
+          >
+            {open ? <FaTimes /> : <FaBars />}
+          </div>
+          {/* link items */}
+          <ul
+            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+              open ? "top-12" : "top-[-490px]"
+            }`}
+          >
+            {Links.map((link) => (
+              <li
+                className="md:ml-8 md:my-0 my-7 font-semibold"
+                key={link.name}
+              >
+                <div className="text-gray-800 hover:text-blue-400 duration-500 cursor-pointer">
+                  {link.name}
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-        {/* Menu icon */}
-        <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
-          {open ? <FaTimes /> : <FaBars />}
-        </div>
-        {/* link items */}
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
-          {Links.map((link) => (
-            <li className='md:ml-8 md:my-0 my-7 font-semibold' key={link.name}>
-              <div className='text-gray-800 hover:text-blue-400 duration-500 cursor-pointer'>{link.name}</div>
-            </li>
-          ))}
-        </ul>
       </div>
-    </div>
-          </>
+    </>
   );
 };
 
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,67 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Todo = () => {
-    
-    const [todo, setTodo] = useState("");
-    const [todoList, setTodoList] = useState([]);
-  
-    const handleForm = (e) => {
-      e.preventDefault();
-      setTodoList([...todoList, { todoName: todo }]);
-      setTodo("");
-    };
-  
-    const deleteTodo = (deleteValue) => {
-      const restTodoList = [
-        ...todoList.filter((val) => {
-          return val.todoName !== deleteValue;
-        }),
-      ];
-      setTodoList(restTodoList);
-    };
-  
-    return (
-      <div className="bg-gray-200 w-full h-screen flex items-center">
-        <div className="w-[500px] mx-auto text-center bg-white p-5">
-          <h1 className="text-5xl font-bold mb-8">Todo List</h1>
-          <form onSubmit={handleForm}>
-            <input
-              className="border-2 placeholder:text-gray-500 rounded-lg border-black w-full p-5 mb-5 text-black"
-              type="text"
-              placeholder="Add Todo"
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="bg-red-600 text-white py-3 px-8 rounded-lg mb-8"
-            >
-              Add Todo
-            </button>
-          </form>
-          <div className="todo-show-area">
-            <ul>
-              {todoList.map((singleTodo, index) => {
-                return (
-                  <li
-                    key={index}
-                    className="bg-black mb-5 flex justify-between  text-white py-5 rounded-lg text-3xl px-5"
+  const [todo, setTodo] = useState(null);
+  const [todoList, setTodoList] = useState([]);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    setTodoList([...todoList, { todoName: todo }]);
+    setTodo('');
+  };
+
+  const deleteTodo = (deleteValue) => {
+    const restTodoList = todoList.filter((val) => val.todoName !== deleteValue);
+    setTodoList(restTodoList);
+  };
+
+  return (
+    <div className="bg-gray-200 w-full h-screen flex items-center">
+      <div className="mx-auto text-center bg-white p-5 w-auto h-auto">
+        <h1 className="text-5xl font-bold mb-8">Todo List</h1>
+        <form onSubmit={handleForm}>
+          <input
+            className="border-2 placeholder:text-gray-500 rounded-lg border-black w-full p-5 mb-5 text-black"
+            type="text"
+            placeholder="Add Todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-red-600 text-white py-3 px-8 rounded-lg mb-8 hover:bg-green-500"
+          >
+            Add Todo
+          </button>
+        </form>
+        <div className="todo-show-area" style={{ maxHeight: '400px', overflowY: 'auto' }}>
+          <ul>
+            {todoList.map((singleTodo, index) => {
+              return (
+                <li
+                  key={index}
+                  className="bg-black mb-5 flex justify-between text-white py-5 rounded-lg text-3xl px-5"
+                >
+                  {singleTodo.todoName}{' '}
+                  <span
+                    onClick={() => deleteTodo(singleTodo.todoName)}
+                    className="text-red-600 cursor-pointer"
                   >
-                    {singleTodo.todoName}{" "}
-                    <span
-                      onClick={() => deleteTodo(singleTodo.todoName)}
-                      className="text-red-600 cursor-pointer"
-                    >
-                      x
-                    </span>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+                    x
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
